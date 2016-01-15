9509eb701b3e003027be2c01ec016a658abb4285
Steven <sstevenwwilliams@aol.com>
2016-01-15 13:44:07 -0500

    controller update

--- listingController.js
+++ listingController.js
@@ -2,13 +2,30 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
-
+	$scope.newItem ={};
+   
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+		  $scope.listings.push($scope.newItem);
+		  $scope.newItem={};
+	};
+	
+    $scope.deleteListing = function(index) {
+		$scope.listings.splice(index,1);
+	};
+	
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = 'Name: ' + $scope.listings[index].name + '\n' +
+	                        'Code: ' + $scope.listings[index].code + '\n';
+	  if($scope.listings[index].coordinates!=undefined){
+							$scope.detailedInfo += 'Address: '+ $scope.listings[index].address + '\n' +
+							'Latitude: ' + $scope.listings[index].coordinates.latitude + '\n' +
+							'Longitude: ' +$scope.listings[index].coordinates.longitude;
+	      }
+	}
+	
   }
 ]);
